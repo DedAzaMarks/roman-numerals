e8d1c0f0a3e8b5b175454b67524ea8f56551cd97
DedAzaMarks <maxmutant1@yandex.ru>
2021-06-20 18:30:03 +0300

    switch to vector<string>, output iff fits classic notation, Google code style

--- Convert.h
+++ Convert.h
@@ -1,25 +1,26 @@
 //
 // Created by Maxim Bordyugov on 16/06/2021.
 //
-
 #ifndef ROMAN_NUMERALS_CONVERT_H
 #define ROMAN_NUMERALS_CONVERT_H
 
 #include <string>
+#include <vector>
 
 using std::string;
+using std::vector;
 
 class Convert {
-private:
-    static const string hundreds[10];
-    static const string tens[10];
-    static const string ones[10];
-public:
-    static int symbol(char c);
+ private:
+  static const vector<string> thousands;
+  static const vector<string> hundreds;
+  static const vector<string> tens;
+  static const vector<string> ones;
 
-    static int64_t to_int(string& str);
-
-    static string to_roman(int64_t x);
+ public:
+  static int symbol(char c);
+  static int64_t to_int(string &str);
+  static string to_roman(int64_t x);
 };
 
 #endif //ROMAN_NUMERALS_CONVERT_H
